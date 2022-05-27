8dfcb47b0a53413bd2c180c40f3ff73faa2eac37
rldnrl <devpark1002@gmail.com>
2022-05-27 21:44:01 +0900

    feat: apollo client provider 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/tailwind.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/libs/ui/Layout'
+import { ApolloProvider } from '@apollo/client'
+
+import apolloClient from '@/libs/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   )
 }
 
