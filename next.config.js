176648edd80e320071e200c9e085d70ebfbdfd7c
rldnrl <devpark1002@gmail.com>
2022-05-28 10:44:05 +0900

    feat: next.config.js에 도메인 추가.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'nextjs.org',
+      'www.prisma.io',
+      'tailwindcss.com',
+      'www.apollographql.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
