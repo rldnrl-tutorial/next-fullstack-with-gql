b11d40c9d371936e49f74b129708669d8f0ccd2d
rldnrl <devpark1002@gmail.com>
2022-06-08 21:48:10 +0900

    feat: home 컴포넌트 구현 중(wip)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,53 @@
-import type { NextPage } from 'next'
+// /pages/index.tsx
+import Head from 'next/head'
+import { gql, useQuery } from '@apollo/client'
+import { Link } from '@prisma/client'
+import Card from '@/libs/ui/Card'
 
-const Home: NextPage = () => {
-  return <div>Hello World</div>
+const AllLinksQuery = gql`
+  query {
+    links {
+      id
+      title
+      url
+      description
+      imageUrl
+      category
+    }
+  }
+`
+
+export default function Home() {
+  const { data, loading, error } = useQuery(AllLinksQuery)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Oh no... {error.message}</p>
+
+  return (
+    <div>
+      <Head>
+        <title>Awesome Links</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto max-w-5xl my-20">
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data.links.map(
+            ({ id, imageUrl, title, category, description, url }: Link) => (
+              <li key={id} className="shadow max-w-md rounded">
+                <Card
+                  imageUrl={imageUrl}
+                  alt={title}
+                  title={title}
+                  description={description}
+                  category={category}
+                  to={url}
+                />
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+    </div>
+  )
 }
-
-export default Home
