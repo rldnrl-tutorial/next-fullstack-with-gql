dd3a6ff9244aa286d4a1cfec0573ffc64cda7988
rldnrl <devpark1002@gmail.com>
2022-06-12 16:44:19 +0900

    feat: set up react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import '../styles/tailwind.css'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+
 import Layout from '@/libs/ui/Layout'
-import { ApolloProvider } from '@apollo/client'
+import { useState } from 'react'
 
-import apolloClient from '@/libs/apollo'
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
 
-function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
-
-export default MyApp
